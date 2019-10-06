569cacefbe26437fd128fde90364a89e6b89abf8
Pratima Gautam <astute.yard@gmail.com>
2019-10-06 08:35:45 +0545

    Added backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+// express, cors, body-parser, jwt, mysql, bcrypt, @hapi/joi, sequelize, nodemon,
+const express = require('express');
+const  user = require('./routes/userRoute');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/api/user', user);
+
+
+app.listen(3000, console.log('Server started at port 3000'));
