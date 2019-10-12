db61a0092dc7cfd4b499ec0792ac7fe0184ee80c
Pratima Gautam <astute.yard@gmail.com>
2019-10-12 15:46:26 +0545

    Making route for students and staffs

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 // express, cors, body-parser, jwt, mysql, bcrypt, @hapi/joi, sequelize, nodemon,
 const express = require('express');
-const  user = require('./routes/userRoute');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -17,5 +16,13 @@ app.use(bodyParser.json())
 // making route for institute
 var Institutes = require("./routes/Institutes");
 app.use("/institutes", Institutes);
+
+// making route for staff
+var Staffs = require("./routes/Staffs");
+app.use("/staffs", Staffs);
+
+// making route for student
+var Students = require("./routes/Students");
+app.use("/students", Students);
 
 app.listen(3000, console.log('Server started at port 3000'));
