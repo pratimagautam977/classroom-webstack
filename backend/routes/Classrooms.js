5681c2b20506d98dfb9a308c11888f63895786a4
Pratima Gautam <astute.yard@gmail.com>
2019-10-16 07:51:50 +0545

    Middleware Implementation and updating the names

--- backend/routes/Classrooms.js
+++ backend/routes/Classrooms.js
@@ -5,12 +5,16 @@ const Classroom = require("../models/classroom");
 const uuidv4 = require('uuid/v4');
 classrooms.use(cors());
 
+// ########  MIDDLEWARE   ########
+const midddleware = require('../config/Middleware');    //Added Middleware
+// ###############################
+
 // POST Route 
-classrooms.post("/", (req, res) => {
+classrooms.post("/", midddleware.checkToken, (req, res) => {
     const classData = {
-        class_uuid: "",
-        ins_uuid: "035f93a5-2f92-4cc1-9668-3103da7ce5e8",
-        class_name: req.body.name
+        ID: "",
+        ins_uuid: req.decoded.id,
+        name: req.body.name
     }
 
     classData.class_uuid = uuidv4();
@@ -26,12 +30,12 @@ classrooms.post("/", (req, res) => {
 })
 
 // GET Route
-classrooms.get("/", (req, res) => {
+classrooms.get("/", midddleware.checkToken, (req, res) => {
     Classroom.findAll({
         where: {
-            ins_uuid: "035f93a5-2f92-4cc1-9668-3103da7ce5e8"
+            ins_uuid: req.decoded.id
         },
-        attributes: ['class_uuid', 'class_name', 'class_created_at']
+        attributes: ['ID', 'name', 'createdAt']
     })
     .then(classroom => {
         res.status(200).json({status: "OK", classroom})
@@ -39,6 +43,44 @@ classrooms.get("/", (req, res) => {
     .catch(err => {
         res.send(err);
     })
-
 })
+
+// GET Route to retrieve a single classroom <findOne>
+classrooms.get("/:id", midddleware.checkToken, (req, res) => {
+    Classroom.findOne({
+        where: {
+            ins_uuid: req.decoded.id,
+            class_uuid: req.params.id
+        },
+        attributes: ['ID', 'name', 'createdAt']
+    })
+    .then(classroom => {
+        res.status(200).json({status: "Ok", classroom})
+    })
+    .catch(err => {
+        res.send(err);
+    })
+})
+
+// PUT Route <update>
+classrooms.put('/:id', (req, res) => {
+    //for now lets skip update and start with delete 
+})
+
+// DELETE Route <drop>
+classrooms.delete('/:id', midddleware.checkToken, (req, res) => {
+    Classroom.destroy({
+        where: {
+            class_uuid : req.params.id, 
+            ins_uuid: req.decoded.id
+        }
+    })
+    .then(classroom => {
+        res.status(200).json({status: "Ok"})
+    })
+    .catch(err => {
+        res.send(err);
+    })
+})
+
 module.exports = classrooms;
