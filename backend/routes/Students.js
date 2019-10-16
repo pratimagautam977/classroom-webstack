32e593e9ed04ca654b6f5ec3e2e944cb31f6209e
Pratima Gautam <astute.yard@gmail.com>
2019-10-16 07:51:26 +0545

    Middleware Implementation and updating the names

--- backend/routes/Students.js
+++ backend/routes/Students.js
@@ -7,32 +7,37 @@ const Student = require("../models/student");
 const uuidv4 = require('uuid/v4');
 students.use(cors());
 
-// POST Registration Route 
-students.post("/", (req, res) => {
+
+// ########  MIDDLEWARE   ########
+const midddleware = require('../config/Middleware');    //Added Middleware
+// ###############################
+
+// POST Registration Route (Add Student)
+students.post("/", midddleware.checkToken, (req, res) => {
     const studentData = {
-        student_uuid: "",
-        ins_uuid: "035f93a5-2f92-4cc1-9668-3103da7ce5e8",
-        stud_fname: req.body.fname,
-        stud_lname: req.body.lname,
-        stud_email: req.body.email,
-        stud_address: req.body.address,
-        stud_phone: req.body.phone,
-        stud_img: "https://icon-library.net/images/default-profile-icon/default-profile-icon-24.jpg",
-        stud_username: req.body.username,
-        stud_password: req.body.password,
+        uuid: "",
+        ins_uuid: req.decoded.id,
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        address: req.body.address,
+        phone: req.body.phone,
+        img: "https://icon-library.net/images/default-profile-icon/default-profile-icon-24.jpg",
+        username: req.body.username,
+        password: req.body.password,
     }
 
     Student.findOne({
         where: {
-            stud_email: req.body.email,
+            email: req.body.email,
             ins_uuid: studentData.ins_uuid
         }
     })
     .then(student => {
         if(!student){
             bcrypt.hash(req.body.password, 10, (err, hash) => {
-                studentData.stud_password = hash;
-                studentData.stud_uuid = uuidv4();
+                studentData.password = hash;
+                studentData.uuid = uuidv4();
                 Student.create(studentData)
                 .then(student => {
                     res.status(200).json({
@@ -55,12 +60,12 @@ students.post("/", (req, res) => {
 })
 
 // GET ALL Students
-students.get("/", (req, res) => {
+students.get("/", midddleware.checkToken, (req, res) => {
     Student.findAll({
         where: {
-            ins_uuid: '035f93a5-2f92-4cc1-9668-3103da7ce5e8'
+            ins_uuid: req.decoded.id
         },
-        attributes: ['stud_uuid', 'stud_fname', 'stud_lname' , 'stud_email', 'stud_phone', 'stud_img']
+        attributes: ['uuid', 'fname', 'lname' , 'email', 'phone', 'img']
     })
     .then(student => {
         res.status(200).json({status: "OK", student})
@@ -71,13 +76,13 @@ students.get("/", (req, res) => {
 })
 
 // GET Route to retrieve a single student <findOne>
-students.get("/:id", (req, res) => {
+students.get("/:id", midddleware.checkToken, (req, res) => {
     Student.findOne({
         where: {
-            ins_uuid:'035f93a5-2f92-4cc1-9668-3103da7ce5e8',
-            stud_uuid: req.params.id
+            ins_uuid:req.decoded.id,
+            uuid: req.params.id
         },
-        attributes: ['stud_uuid', 'stud_fname', 'stud_lname' , 'stud_email', 'stud_phone', 'stud_address', 'stud_img']
+        attributes: ['uuid', 'fname', 'lname' , 'email', 'phone', 'address', 'img']
     })
     .then(student => {
         res.status(200).json({status: "Ok", student})
@@ -91,13 +96,13 @@ students.get("/:id", (req, res) => {
 students.post('/login', (req, res) => {
     Student.findOne({
         where: {
-            stud_email: req.body.email
+            email: req.body.email
         }
     })
     .then(student => {
         if (student) {
-            if(bcrypt.compareSync(req.body.password, student.stud_password)){
-                let token = jwt.sign({id: student.stud_uuid}, "icp-student", {
+            if(bcrypt.compareSync(req.body.password, student.password)){
+                let token = jwt.sign({id: student.uuid, isAdmin: false, isStaff: false, isStudent: true}, process.env.APP_SECRET, {
                     expiresIn: 1440
                 })
                 res.send({token});
