496d125eab6600f86b9a01a47c1ec9660b17e15a
Pratima Gautam <astute.yard@gmail.com>
2019-11-04 22:15:23 +0545

    Added Sidebar Component

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="_logo">
+                <img src="./images/logo_m.png" alt="classroom"/>
+            </div>
+            <div className="item">
+                <NavLink to="/student" activeClassName="selected"><span className="icon-cap"></span></NavLink>
+            </div>
+            <div className="item">
+                <NavLink to="/staff" activeClassName="selected"><span className="icon-hired"></span></NavLink>
+            </div>
+            <div className="item">
+                <NavLink to="/classroom" activeClassName="selected"><span className="icon-blackboard"></span></NavLink>
+            </div>
+            <div className="item">
+                <NavLink to="/idea" activeClassName="selected"><span className="icon-idea"></span></NavLink>
+            </div>
+            <div className="item">
+                <NavLink to="/broadcast" activeClassName="selected"><span className="icon-promotion"></span></NavLink>
+            </div>
+            <div className="item">
+                <NavLink to="/files" activeClassName="selected"><span className="icon-file"></span></NavLink>
+            </div>
+            <div className="item">
+                <NavLink to="/messenger" activeClassName="selected"><span className="icon-conversation"></span></NavLink>
+            </div>
+            <div className="item">
+                <NavLink to="/calendar" activeClassName="selected"><span className="icon-calendar"></span></NavLink>
+            </div>
+        </div>
+    )
+}
+
