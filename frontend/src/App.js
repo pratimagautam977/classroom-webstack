93d9cabdfd7ba427617ce2f1dd3130851f8510a0
Pratima Gautam <astute.yard@gmail.com>
2019-12-11 14:48:32 +0545

    Imported HashRouter from react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
