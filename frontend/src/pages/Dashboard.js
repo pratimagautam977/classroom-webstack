7cccc410919b5dfc4bbbd6437da161f4fb960ee6
Pratima Gautam <astute.yard@gmail.com>
2019-12-11 15:20:50 +0545

    Modified Dashboard.js

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,55 +1,30 @@
 import React from 'react';
-import {Helmet} from 'react-helmet';
-import NotFound from './NotFound';
-import Classroom from './Classroom';
-import Student from './Student';
-import Staff from './Staff';
-import Sidebar from '../components/Sidebar';
-import Topbar from '../components/Topbar';
-import { Route, Switch } from "react-router-dom";
-import Payment from './Payment';
+import jwt_decode from 'jwt-decode';
+import Institute from './Admin/Dashboard'
+import Staffs from './Staff/Dashboard';
+import {Redirect, withRouter } from "react-router-dom";
 
-class Dashboard extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            profile: "./images/profile.png"
+function Dashboard(props) {
+    // now install a jwt parse package
+   
+    const token = localStorage.getItem("token");
+    if (token !== null){
+        const decoded = jwt_decode(token)
+        if(decoded.isAdmin === true){
+            return <Institute />
+        }
+        else if(decoded.isStaff === true){
+            return <Staffs />
+        }
+        else if(decoded.isStudent === true){
+            return "Dear Students :) We are soon bring service for you. -By your respectful Admin"
+        }
+        else{
+            return <Redirect to='/login'/>;
         }
     }
-    render(){
-        return(
-            <React.Fragment>
-                <Helmet>
-                    <title>Classroom WebStack | Dashboard</title>
-                </Helmet>
-
-                <div className="dashboard">                
-                    <div className="container-fluid">            
-                        <Sidebar/>
-                        <div className="mainbar">
-                            <Topbar data={this.state.profile}/>             
-                            <div className="main_body">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-lg-12 col-md-12 col-sm-12">
-                                            <Switch>
-                                            <Route path="/classroom" component={Classroom}/>
-                                            <Route path="/home" component={Classroom}/>
-                                            <Route path="/staff" component={Staff} />
-                                            <Route path="/student" component={Student}/> 
-                                            <Route path="/payment" component={Payment}/>
-                                            <Route component={NotFound}/> 
-                                            </Switch>
-                                        </div>
-                                    </div>
-                                </div>                            
-                            </div>
-                        </div>       
-                    </div>       
-                </div>                                             
-            </React.Fragment>
-        );
+    else{
+        return <Redirect to='/login'/>;
     }
 }
-
-export default Dashboard;
+export default withRouter(Dashboard);
