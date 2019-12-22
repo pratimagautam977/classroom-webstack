6b4f75e85852f3bc05f4420580cfbc1cd0865b9d
Pratima Gautam <astute.yard@gmail.com>
2019-12-22 18:34:15 +0545

    Modified ClassroomView

--- frontend/src/pages/Admin/ClassroomView.js
+++ frontend/src/pages/Admin/ClassroomView.js
@@ -1,40 +1,17 @@
 import React, { useState, useEffect } from "react";
-//u also need state dear to store the JSON returns
+//need state to store the JSON returns
 import { useParams } from "react-router-dom";
-import { GetClass } from "../../components/classroom/ClassroomFunction";
+import { GetClass, GetAssignment } from "../../components/classroom/ClassroomFunction";
 
 export default function ClassroomView() {
-  const [ClassData, setClassData] = useState({});
-  let { id } = useParams();
-
-  useEffect(() => {
-    GetClass(id).then(res => {
-      setClassData(res);
-    });
-  }, []);
-  let staff = ClassData.staffs;
-  let student = ClassData.students;
   
-  return (
-    <div>
+  const feeds = () => {
+    return (
       <div className="row">
-        <div className="col-12">
-          <div
-            className="cards hg-100"
-            style={{ background: `url(${ClassData.img})` }}
-          >
-            <div className="p-5 h-w-b">
-              <h2>{ClassData.name}</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="row">
-          <div className="col-12 px-3">
+        <div className="col-12 px-3">
             <form className="col-12 cards" >
               <p className="lead emoji-picker-container" style={{marginTop:"35px"}}>
-                <textarea className=" col-12 form-control textarea-control" contentEditable="true" rows="2" placeholder="Share with your class" data-emojiable="true"></textarea>
+                <textarea className=" col-12 form-control textarea-control" suppressContentEditableWarning={true} rows="2" placeholder="Share with your class" data-emojiable="true"></textarea>
               </p>
               <div className="footer d-inline">
                 <div className="footer__btn pr-2">
@@ -44,16 +21,16 @@ export default function ClassroomView() {
                   <button className="footer_btn mr-3"><span className="icon-folder"></span></button>
                 </div>
               </div>
-
             </form> 
         </div>
       </div>
-
+    )
+  }
+  const people = () => {    
+    return (
       <div className="row">
         <div className="col-lg-4 col-md-6 col-sm-12">
-          <div className="cards p-3 hg-400">
-
-            
+          <div className="cards p-3 hg-400">   
             <h5 style={{display: "inline-block", width: "calc(100% - 32px)"}}>Staffs</h5>
             <button className="btn btn-primary btn-sm pb-4" style={{display: "inline-block"}}><span className="icon-add-user"></span></button>
 
@@ -70,21 +47,90 @@ export default function ClassroomView() {
             ))}
           </div>
         </div>
+
         <div className="col-lg-4 col-md-6 col-sm-12">
           <div className="cards p-3 hg-400">
             <h5 style={{display: "inline-block", width: "calc(100% - 32px)"}}>Students</h5>
               <button className="btn btn-primary btn-sm pb-4" style={{display: "inline-block"}}><span className="icon-add-user"></span></button>
-            {student && student.map(cl => (
-              <h6>{cl.fname} {cl.lname}</h6>
+            {student && student.map((cl, index) => (
+              <h6 key={index}>{cl.fname} {cl.lname}</h6>
             ))}
           </div>
         </div>
+
         <div className="col-lg-4 col-md-6 col-sm-12">
           <div className="cards p-3 hg-400">
             <h5>Upcoming Events</h5>
           </div>
         </div>
+    
       </div>
+    )
+  }
+  const assignments = () => {
+    return (
+    <div className="row">
+      <div className="col-12">
+
+        {AssignmentData.map(ass => (
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{ass.name}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">{ass.staff_name}</h6>
+              <small>{ass.assignedAt}</small>
+              <p className="card-text">{ass.details}</p>
+            </div>
+          </div>
+        ))} 
+      </div>
+    </div>
+    )
+  }
+
+  const [ClassData, setClassData] = useState({});
+  const [AssignmentData, setAssignmentData] = useState({});
+  const [View, setView] = useState(feeds);
+  // const [Toggle, setToggle] = useState("feeds");
+  let { id } = useParams();
+
+  useEffect(() => {
+    GetClass(id).then(res => {
+      setClassData(res);
+    });
+
+    GetAssignment(id).then(res => {
+      setAssignmentData(res);
+    })
+  }, [id]);
+
+  let staff = ClassData.staffs;
+  let student = ClassData.students;
+  
+  const changeView = (view) => {
+    setView(view);
+    // setToggle(view);
+  }
+
+  return (
+    <div>
+      <div className="row">
+        <div className="col-12">
+          <div
+            className="cards hg-100"
+            style={{ background: `url(${ClassData.img})` }}
+          >
+            <div className="p-5 h-w-b">
+              <h2>{ClassData.name}</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <button className="mr-2 mb-3 btn-primary btn pb-4" onClick={() => changeView(feeds)}>Feeds</button>
+      <button className="mr-2 mb-3 btn-primary btn pb-4" onClick={() => changeView(people)}>People</button>
+      <button className="mr-2 mb-3 btn-primary btn pb-4" onClick={() => changeView(assignments)}>Assignments</button>
+      {View}
+
     </div>
   );
 }
