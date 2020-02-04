b25cb2ab9b2c634e3a8062df92e5c11c92b558a6
Pratima Gautam <astute.yard@gmail.com>
2020-02-04 23:28:50 +0545

    Updated Sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export default function Sidebar() {
+export default function Sidebar(props) {
+    if(props.value === 'ins'){
+    
     return (
         <div className="sidebar">
             <div className="_logo">
@@ -33,5 +35,33 @@ export default function Sidebar() {
             </div>
         </div>
     )
+    }
+    else if(props.value === 'staff'){
+        return (
+            <div className="sidebar">
+                <div className="_logo">
+                    <img src="./images/logo_m.png" alt="classroom"/>
+                </div>
+                <div className="item">
+                    <NavLink to="/classroom" activeClassName="selected"><span className="icon-blackboard"></span></NavLink>
+                </div>
+                <div className="item">
+                    <NavLink to="/student" activeClassName="selected"><span className="icon-cap"></span></NavLink>
+                </div>
+                <div className="item">
+                    <NavLink to="/staff" activeClassName="selected"><span className="icon-hired"></span></NavLink>
+                </div>
+                <div className="item">
+                    <NavLink to="/file" activeClassName="selected"><span className="icon-file"></span></NavLink>
+                </div>
+                <div className="item">
+                    <NavLink to="/chat" activeClassName="selected"><span className="icon-conversation"></span></NavLink>
+                </div>
+                <div className="item">
+                    <NavLink to="/calendar" activeClassName="selected"><span className="icon-calendar"></span></NavLink>
+                </div>
+            </div>
+        )
+    }
 }
 
