fc3f8665d6373fb11d366d03e07b2c07ff0f7302
Pratima Gautam <astute.yard@gmail.com>
2020-03-07 13:33:44 +0545

    Added Payment.js model

---
+++ backend/models/payment.js
@@ -0,0 +1,44 @@
+const Sequelize = require("sequelize");
+const db = require("../config/config");
+
+
+module.exports = db.sequelize.define(
+    'tbl_payment',
+    {
+        payment_id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        studID: {
+            type: Sequelize.UUIDV4,
+            field: 'stud_uuid'
+        },
+        requestID: {
+            type:Sequelize.UUIDV4 ,
+            field: 'request_id'
+        },
+        requestDate: {
+            type: Sequelize.DATE,
+            field: 'request_date'
+        },
+        amount: {
+            type: Sequelize.STRING
+        },
+        insID: {
+            type: Sequelize.STRING,
+            field: 'ins_uuid'
+        },
+        status: {
+            type: Sequelize.BOOLEAN
+        }
+    },    
+
+    {
+        // disable the modification of tablenames; By default, sequelize will automatically
+        // transform all passed model names (first parameter of define) into plural.
+        // if you don't want that, set the following
+        freezeTableName: true,
+        tableName: 'tbl_payment'
+    }
+)
