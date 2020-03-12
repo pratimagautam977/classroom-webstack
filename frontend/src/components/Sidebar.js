ae44d2601a155d392ab7ee5132b638fbbc667be3
Pratima Gautam <astute.yard@gmail.com>
2020-03-12 15:37:49 +0545

    Added stud props in sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -63,5 +63,33 @@ export default function Sidebar(props) {
             </div>
         )
     }
+
+    else if(props.value === 'stud'){
+        return(
+            <div className="sidebar">
+                <div className="_logo">
+                    <img src="./images/logo_m.png" alt="classroom"/>
+                </div>
+                <div className="item">
+                    <NavLink to="/classroom" activeClassName="selected"><span className="icon-blackboard"></span></NavLink>
+                </div>
+                <div className="item">
+                    <NavLink to="/student" activeClassName="selected"><span className="icon-cap"></span></NavLink>
+                </div>
+                <div className="item">
+                    <NavLink to="/staff" activeClassName="selected"><span className="icon-hired"></span></NavLink>
+                </div>
+                <div className="item">
+                    <NavLink to="/file" activeClassName="selected"><span className="icon-file"></span></NavLink>
+                </div>
+                <div className="item">
+                    <NavLink to="/chat" activeClassName="selected"><span className="icon-conversation"></span></NavLink>
+                </div>
+                <div className="item">
+                    <NavLink to="/calendar" activeClassName="selected"><span className="icon-calendar"></span></NavLink>
+                </div>
+            </div>
+        )
+    }
 }
 
