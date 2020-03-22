9a97131fc8dc27f5c55c39a5230d7df444affb7f
Pratima Gautam <astute.yard@gmail.com>
2020-03-22 22:50:28 +0545

    Added Classroom.js to show all the classroom where students were added

---
+++ frontend/src/pages/Student/Classroom.js
@@ -0,0 +1,66 @@
+//rcc => for class-based component
+import React, { Component } from "react";
+import axios from "axios";
+import { Helmet } from "react-helmet";
+import AllClass from "../../components/classroom/allClass";
+
+export default class Classroom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: "",
+      classes: [],
+      load: false
+    };
+  }
+
+  componentDidMount() {
+    const t = localStorage.getItem("token");
+    setTimeout(
+      function() {
+        axios
+          .get(`http://localhost:3000/student/classroom`, {
+            headers: { Authorization: `Bearer ${t}` }
+          })
+          .then(res => {
+            if (res.status === 200) {
+              const classes = res.data;
+              this.setState({ classes, load: true });
+            } else {
+              this.setState({ error: res.data.error });
+              this.props.history.push("/login");
+            }
+          })
+          .catch(err => {
+            this.setState({ error: err.response, load: true });
+          });
+      }.bind(this),
+      1000
+    );
+  }  
+
+  render() {
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>Classroom WebStack | Classroom</title>
+        </Helmet>
+        {this.state.load ? (
+          <AllClass data={this.state.classes} />
+        ) : (
+          <div
+            style={{
+              height: "90vh",
+              display: "flex",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <div className="loader" id="loader-1"></div>
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
+}
