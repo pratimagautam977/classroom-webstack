2895641101289912662a0a37732b4bd56c99d5ba
Pratima Gautam <astute.yard@gmail.com>
2020-03-29 02:56:57 +0545

    Added update assignment pages with the added functionality to update the assignment on submit

--- frontend/src/pages/Staff/Assignment.js
+++ frontend/src/pages/Staff/Assignment.js
@@ -3,17 +3,29 @@ import {
   PostAssignment,
   GetAssignment
 } from "../../components/classroom/ClassroomFunction";
-import jwt_decode from 'jwt-decode';
+import jwt_decode from "jwt-decode";
 
 export default function Assignment(props) {
   const t = localStorage.getItem("token");
   const decoded_token = jwt_decode(t);
-  console.log(decoded_token.login)
+  console.log(decoded_token.login);
 
   const [assign, setAssign] = useState({
     names: "",
     details: ""
   });
+
+  const [updateAssignment, setUpdateAssignment] = useState({
+    names: "",
+    details: "",
+    id: ""
+  });
+
+  const onUpdateAssignement = e => {
+    setUpdateAssignment({...updateAssignment, [e.target.name]: e.target.value})
+  }
+
+  const [key, setKey] = useState();
 
   const onAssignChange = e => {
     setAssign({ ...assign, [e.target.name]: e.target.value });
@@ -35,6 +47,12 @@ export default function Assignment(props) {
       .catch(err => {});
   };
 
+  const onUpdateAssignmentSubmit = (e) => {
+    e.preventDefault();
+    props.onUpdateAssignment(updateAssignment);
+    setKey();
+  }
+
   return (
     <div className="row">
       <div className="col-12">
@@ -55,7 +73,7 @@ export default function Assignment(props) {
                       data-emojiable="true"
                       name="names"
                       onChange={onAssignChange}
-                      value={assign.names}
+                      value={assign.names }
                     ></input>
                   </div>
                   <div className="col-4">
@@ -94,16 +112,66 @@ export default function Assignment(props) {
         </div>
 
         {props.AssignmentData &&
-          props.AssignmentData.map((ass,index) => (
-            <div className="card" key={index}>
+          props.AssignmentData.map((ass, index) => (
+            <div className="card" key={index+1}>
               <div className="card-body">
-                <h5 className="card-title">{ass.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  {ass.staff_name}
-                </h6>
-                <small>{ass.assignedAt}</small>
-                <p className="card-text">{ass.details}</p>
-                {decoded_token.login === ass.StaffID ? <button className="btn btn-primary pb-4" onClick={() => props.onDeleteAssignment(ass.id)}>Delete</button> : ""}
+                {key === index+1 ? (
+                  <form onSubmit={onUpdateAssignmentSubmit}>
+                    <input
+                      required
+                      type="text"
+                      className="col-12 form-control mb-2"
+                      placeholder="Assignment Title"
+                      data-emojiable="true"
+                      name="names"
+                      onChange={onUpdateAssignement}
+                      value={updateAssignment.names}
+                    ></input>
+
+                    <textarea
+                      required
+                      className="col-12 form-control mb-3"
+                      suppressContentEditableWarning={true}
+                      rows="2"
+                      placeholder="Share with your class"
+                      data-emojiable="true"
+                      name="details"
+                      onChange={onUpdateAssignement}
+                      value={updateAssignment.details}
+                    ></textarea>
+
+                    <button className="btn btn-success mr-2 pb-4" type="submit">Update</button>
+                    <button className="btn btn-danger pb-4" onClick={() => setKey(0)}>Cancel</button>
+                  </form>
+                ) : (
+                  <>
+                    <h5 className="card-title">{ass.name}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      {ass.staff_name}
+                    </h6>
+                    <small>{ass.assignedAt}</small>
+                    <p className="card-text">{ass.details}</p>
+                    {decoded_token.login === ass.StaffID ? (
+                      <>
+                        <button
+                          className="btn btn-danger pb-4 mr-2"
+                          onClick={() =>props.onDeleteAssignment(ass.id)}
+                        >
+                          Delete
+                        </button>
+
+                        <button
+                          className="btn btn-primary pb-4"
+                          onClick={() => {setKey(index+1); setUpdateAssignment({names: ass.name, details: ass.details, id: ass.id});}}
+                        >
+                          Edit
+                        </button>
+                      </>
+                    ) : (
+                      ""
+                    )}
+                  </>
+                )}
               </div>
             </div>
           ))}
